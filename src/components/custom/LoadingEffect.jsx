cc66e6385ebc07d2480780fa255423be1823dc94
Fardin Faiaz Khan <fardinfaiaz5@gmail.com>
2025-07-21 07:01:51 +0000

    added axios,loader and toaster

---
+++ src/components/custom/LoadingEffect.jsx
@@ -0,0 +1,11 @@
+import '../css/loader.css';
+
+export const LoadingEffect = () => {
+    return ( 
+        <div className="LoadingOverlay">
+          <div className="Line-Progress">
+            <div className="indeterminate"></div>
+          </div>
+        </div>
+     );
+}
--- src/pages/admin/AddStaff.jsx
+++ src/pages/admin/AddStaff.jsx
@@ -1,4 +1,60 @@
+import axios from "axios";
+import { useState } from "react";
+
 export const AddStaff = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [taxFileNumber, setTaxFileNumber] = useState("");
+  const [dob, setDob] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [positionRank, setPositionRank] = useState("");
+  const [employmentType, setEmploymentType] = useState("");
+  const [weeklySalary, setWeeklySalary] = useState("");
+  const [mobileNo, setMobileNo] = useState("");
+  const [emergencyContactName, setEmergencyContactName] = useState("");
+  const [emergencyContactPhone, setEmergencyContactPhone] = useState("");
+  const [residentialAddress, setResidentialAddress] = useState("");
+
+const staffData = {
+  full_name: fullName,
+  email: email,
+  tax_file_number: taxFileNumber,
+  dob: dob,
+  password: loginPassword,
+  rank: positionRank,
+  employment_type: employmentType,
+  amount: weeklySalary,
+  phone_no: mobileNo,
+  ec_name: emergencyContactName,
+  ec_phone_number: emergencyContactPhone,
+  residential_address: residentialAddress,
+  access_level: "Staff" // Example access level
+};
+
+
+  const createNewStaff=(event)=>{
+    event.preventDefault()
+    console.log(staffData)
+axios.post('http://fardin-mise-en.spentry.tech/api/save-staff', staffData, {
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  withCredentials: true
+})
+.then(res => {
+  console.log("✅ Success:", res.data);
+})
+.catch(err => {
+  if (err.response) {
+    console.error("❌ API Error:", err.response.data);
+  } else {
+    console.error("❌ Network Error:", err.message);
+  }
+});
+  }
+
+
   return (
     <div className="bg-gray-100 min-h-screen font-sans">
       <div className="bg-white shadow-md rounded-lg p-4 md:p-8 max-w-6xl mx-auto my-8 pb-18">
@@ -9,21 +65,23 @@ export const AddStaff = () => {
         </div>
 
         <div className="flex-shrink-0 flex flex-col items-center justify-center md:justify-start gap-4 p-4 md:p-0">
-          <div class="profile-avatar rounded-full overflow-hidden bg-gray-200 flex items-center justify-center">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="h-26 w-26 text-gray-500" viewBox="0 0 20 20"
-    fill="currentColor"
-  >
-    <path
-      fillRule="evenodd"
-      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-      clipRule="evenodd"
-    />
-  </svg>
-</div>
+          <div className="profile-avatar rounded-full overflow-hidden bg-gray-200 flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-26 w-26 text-gray-500"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                clipRule="evenodd"
+              />
+                
+            </svg>
+          </div>
           <button className="bg-orange-500 text-white px-6 py-2 rounded-md hover:bg-orange-600 transition-colors text-sm">
-           Upload
+            Upload
           </button>
           <button className="bg-orange-500 text-white px-6 py-2 rounded-md hover:bg-orange-600 transition-colors text-sm">
             Select Access Control Level*
@@ -32,180 +90,201 @@ export const AddStaff = () => {
 
         <div className="flex flex-col md:flex-row gap-8">
           <div className="flex-1 grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4">
-
-            {/* Full Name */}
-            <div>
-              <label htmlFor="Full Name" className="block text-gray-700 mb-1">
-                Full Name
-              </label>
-              <input
-                type="text"
-                name="Full Name"
-                id="Full Name"
-                required
-                placeholder="Full Name"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Email */}
-            <div>
-              <label htmlFor="email" className="block text-gray-700 mb-1">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Tax File Number */}
-            <div>
-              <label htmlFor="taxFileNumber" className="block text-gray-700 mb-1">
-                Tax File Number
-              </label>
-              <input
-                type="text"
-                id="taxFileNumber"
-                name="taxFileNumber"
-                placeholder="Tax File Number"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Date of Birth */}
-            <div>
-              <label htmlFor="dob" className="block text-gray-700 mb-1">
-                Date of Birth
-              </label>
-              <input
-                type="date"
-                id="dob"
-                name="dob"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Login Password */}
-            <div>
-              <label htmlFor="loginPassword" className="block text-gray-700 mb-1">
-                Login Password
-              </label>
-              <input
-                type="password"
-                id="loginPassword"
-                name="loginPassword"
-                placeholder="Password"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Position / Rank */}
-            <div>
-              <label htmlFor="positionRank" className="block text-gray-700 mb-1">
-                Position / Rank
-              </label>
-              <input
-                type="text"
-                id="positionRank"
-                name="positionRank"
-                placeholder="Position / Rank"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Employment Type */}
-            <div>
-              <label htmlFor="employmentType" className="block text-gray-700 mb-1">
-                Employment Type
-              </label>
-              <select
-                id="employmentType"
-                name="employmentType"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              >
-                <option>Select Employment Type</option>
-                <option value="fullTime">Full Time</option>
-                <option value="partTime">Part Time</option>
-                <option value="contract">Contract</option>
-              </select>
-            </div>
-
-            {/* Weekly Salary */}
-            <div>
-              <label htmlFor="weeklySalary" className="block text-gray-700 mb-1">
-                Weekly Salary
-              </label>
-              <input
-                type="text"
-                id="weeklySalary"
-                name="weeklySalary"
-                placeholder="Weekly Salary"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Mobile No. */}
-            <div>
-              <label htmlFor="mobileNo" className="block text-gray-700 mb-1">
-                Mobile No.
-              </label>
-              <input
-                type="text"
-                id="mobileNo"
-                name="mobileNo"
-                placeholder="Mobile No."
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              />
-            </div>
-
-            {/* Emergency Contact */}
-            <div>
-              <label htmlFor="emergencyContact" className="block text-gray-700 mb-1">
-                Emergency Contact
-              </label>
-              <div className="flex gap-2">
-                <input
+            <form onSubmit={createNewStaff}> 
+              {/* Full Name */}
+              <div>
+                <label htmlFor="Full Name" className="block text-gray-700 mb-1">
+                  Full Name
+                </label>
+                <input onChange={(e) => setFullName(e.target.value)}
                   type="text"
-                  id="emergencyContactName"
-                  name="emergencyContactName"
-                  placeholder="Contact Name"
-                  className="w-1/2 px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-                />
-                <input
-                  type="text"
-                  id="emergencyContactPhone"
-                  name="emergencyContactPhone"
-                  placeholder="Contact Phone"
-                  className="w-1/2 px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                  name="Full Name"
+                  id="Full Name"
+                  required
+                  placeholder="Full Name"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
                 />
               </div>
-            </div>
 
-            {/* Residential Address */}
-            <div className="md:col-span-2">
-              <label htmlFor="residentialAddress" className="block text-gray-700 mb-1">
-                Residential Address
-              </label>
-              <textarea
-                id="residentialAddress"
-                name="residentialAddress"
-                rows={2}
-                placeholder="Residential Address"
-                className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
-              ></textarea>
-            </div>
+              {/* Email */}
+              <div>
+                <label htmlFor="email" className="block text-gray-700 mb-1">
+                  Email
+                </label>
+                <input onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                />
+              </div>
+
+              {/* Tax File Number */}
+              <div>
+                <label
+                  htmlFor="taxFileNumber"
+                  className="block text-gray-700 mb-1"
+                >
+                  Tax File Number
+                </label>
+                <input onChange={(e) => setTaxFileNumber(e.target.value)}
+                  type="text"
+                  id="taxFileNumber"
+                  name="taxFileNumber"
+                  placeholder="Tax File Number"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                />
+              </div>
+
+              {/* Date of Birth */}
+              <div>
+                <label htmlFor="dob" className="block text-gray-700 mb-1">
+                  Date of Birth
+                </label>
+                <input onChange={(e) => setDob(e.target.value)}
+                  type="date"
+                  id="dob"
+                  name="dob"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                />
+              </div>
+
+              {/* Login Password */}
+              <div>
+                <label
+                  htmlFor="loginPassword"
+                  className="block text-gray-700 mb-1"
+                >
+                  Login Password
+                </label>
+                <input onChange={(e) => setLoginPassword(e.target.value)}
+                  type="password"
+                  id="loginPassword"
+                  name="loginPassword"
+                  placeholder="Password"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                />
+              </div>
+
+              {/* Position / Rank */}
+              <div>
+                <label
+                  htmlFor="positionRank"
+                  className="block text-gray-700 mb-1"
+                >
+                  Position / Rank
+                </label>
+                <input onChange={(e) => setPositionRank(e.target.value)}
+                  type="text"
+                  id="positionRank"
+                  name="positionRank"
+                  placeholder="Position / Rank"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                />
+              </div>
+
+              {/* Employment Type */}
+              <div>
+                <label
+                  htmlFor="employmentType"
+                  className="block text-gray-700 mb-1"
+                >
+                  Employment Type
+                </label>
+                <select onChange={(e) => setEmploymentType(e.target.value)}
+                  id="employmentType"
+                  name="employmentType"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                >
+                  <option>Select Employment Type</option>
+                  <option value="fullTime">Full Time</option>
+                  <option value="partTime">Part Time</option>
+                  <option value="contract">Contract</option>
+                </select>
+              </div>
+
+              {/* Weekly Salary */}
+              <div>
+                <label
+                  htmlFor="weeklySalary"
+                  className="block text-gray-700 mb-1"
+                >
+                  Weekly Salary
+                </label>
+                <input onChange={(e) => setWeeklySalary(e.target.value)}
+                  type="text"
+                  id="weeklySalary"
+                  name="weeklySalary"
+                  placeholder="Weekly Salary"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                />
+              </div>
+
+              {/* Mobile No. */}
+              <div>
+                <label htmlFor="mobileNo" className="block text-gray-700 mb-1">
+                  Mobile No.
+                </label>
+                <input onChange={(e) => setMobileNo(e.target.value)}
+                  type="text"
+                  id="mobileNo"
+                  name="mobileNo"
+                  placeholder="Mobile No."
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                />
+              </div>
+
+              {/* Emergency Contact */}
+              <div>
+                <label
+                  htmlFor="emergencyContact"
+                  className="block text-gray-700 mb-1"
+                >
+                  Emergency Contact
+                </label>
+                <div className="flex gap-2">
+                  <input onChange={(e) => setEmergencyContactName(e.target.value)}
+                    type="text"
+                    id="emergencyContactName"
+                    name="emergencyContactName"
+                    placeholder="Contact Name"
+                    className="w-1/2 px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                  />
+                  <input onChange={(e) => setResidentialAddress(e.target.value)}
+                    type="text"
+                    id="emergencyContactPhone"
+                    name="emergencyContactPhone"
+                    placeholder="Contact Phone"
+                    className="w-1/2 px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                  />
+                </div>
+              </div>
+
+              {/* Residential Address */}
+              <div className="md:col-span-2">
+                <label
+                  htmlFor="residentialAddress"
+                  className="block text-gray-700 mb-1"
+                >
+                  Residential Address
+                </label>
+                <textarea
+                  id="residentialAddress"
+                  name="residentialAddress"
+                  rows={2}
+                  placeholder="Residential Address"
+                  className="w-full px-4 py-2 border-2 border-white rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 mb-4 shadow-xl"
+                ></textarea>
+              </div>
+              {/* Save Button */}
+              <div className="mt-8 text-center">
+                <button className="btn block mx-auto w-1/2 mt-4 bg-orange-500 text-white shadow-2xl shadow-orange-700">
+                  Save
+                </button>
+              </div>
+            </form>
           </div>
-        </div>
-
-        {/* Save Button */}
-        <div className="mt-8 text-center">
-          <button className="btn block mx-auto w-1/2 mt-4 bg-orange-500 text-white shadow-2xl shadow-orange-700">
-            Save
-          </button>
         </div>
       </div>
     </div>
